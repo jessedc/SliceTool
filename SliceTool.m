e922b9549bb31293e1b9008b2322ba4439d0063e
Jesse Collis <jesse@jcmultimedia.com.au>
2012-03-04 15:06:56 +1100

    Shuffled code around, outputs PNGs

--- SliceTool.m
+++ SliceTool.m
@@ -32,205 +32,218 @@
 #import "NSImage+MGCropExtensions.h"
 #import "util.h"
 
-
 static const char *version = "1.0";
+
+#define kTileSize 512.0f
 
 extern BOOL enable_verbose_output;
 extern BOOL enable_debug_output;
 
 void slice_image(NSImage *image, NSColorSpace *colorSpace, NSString *filename, NSString *sub_path, NSString *sliceName)
 {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    
+  @autoreleasepool
+  {
     NSUInteger imageNum = 0;
     
-    for (CGFloat y=0.0; y<image.size.height; y+=512.0) {
-	for (CGFloat x=0.0; x<image.size.width; x+=512.0) {
-	    imageNum++;
-	    
-	    CGFloat width = fmin(512.0, image.size.width-x);
-	    CGFloat height = fmin(512.0, image.size.height-y);
-	    
-	    NSRect cropRect = NSMakeRect(x, fmax(0.0, image.size.height-(y+512.0)), width, height);
-	    NSImage *cropped = [image imageCroppedInRect:cropRect];
-	    
-	    NSString *outputName = [NSString stringWithFormat:@"%@_%@_%02d.jpg", filename, sliceName, imageNum];
-	    NSString *outputFile = [sub_path stringByAppendingPathComponent:outputName];
-	    NSData *tiffData = [cropped TIFFRepresentation];
-	    
-	    NSBitmapImageRep *bitmapImageRep = [[NSBitmapImageRep imageRepWithData:tiffData]
-						bitmapImageRepByConvertingToColorSpace:colorSpace renderingIntent:NSColorRenderingIntentDefault];
-	    
-	    NSDictionary *properties = nil;
-	    //NSDictionary *properties = [NSDictionary dictionaryWithObjectsAndKeys:
-	    //							//[NSNumber numberWithFloat:0.2], NSImageCompressionFactor,
-	    //							colorProfileData, NSImageColorSyncProfileData,
-	    //							nil];
-	    
-	    [[bitmapImageRep representationUsingType:NSJPEGFileType properties:properties] writeToFile:outputFile atomically:YES];
-	}
+    for (CGFloat y = 0.0f; y<image.size.height; y += kTileSize)
+    {
+      for (CGFloat x = 0.0f; x<image.size.width; x += kTileSize)
+      {
+        imageNum++;
+        
+        CGFloat width = fmin(kTileSize, image.size.width - x);
+        CGFloat height = fmin(kTileSize, image.size.height - y);
+        
+        NSRect cropRect = NSMakeRect(x, fmax(0.0f, image.size.height - (y + kTileSize)), width, height);
+        NSImage *cropped = [image imageCroppedInRect:cropRect];
+        
+        NSString *outputName = [NSString stringWithFormat:@"%@_%@_%02d.jpg", filename, sliceName, imageNum];
+        NSString *outputFile = [sub_path stringByAppendingPathComponent:outputName];
+        NSData *tiffData = [cropped TIFFRepresentation];
+        
+        NSBitmapImageRep *bitmapImageRep = [[NSBitmapImageRep imageRepWithData:tiffData]
+                                            bitmapImageRepByConvertingToColorSpace:colorSpace renderingIntent:NSColorRenderingIntentDefault];
+
+        [[bitmapImageRep representationUsingType:NSPNGFileType properties:nil] writeToFile:outputFile atomically:YES];
+      }
     }
     
-    [pool drain];
+  }
 }
 
 int slice_files_at_path(NSString *sub_path)
 {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    
-    //DLog(@"sub_path: %@", sub_path);
+  @autoreleasepool
+  {
     NSError *error = nil;
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSArray *dirlist = [fileManager contentsOfDirectoryAtPath:sub_path error:&error];
-    if (nil == dirlist) {
-	error_output(@"contentsOfDirectoryAtPath:\"%@\" error: %@", sub_path, error);
+
+    if (nil == dirlist)
+    {
+      error_output(@"contentsOfDirectoryAtPath:\"%@\" error: %@", sub_path, error);
     }
     
-    //DLog(@"dirList: %@", dirlist);
-    
-    for (NSString *filename in dirlist) {
-	if ([filename hasPrefix:@"."]) {
-	    continue;
-	}
-	
-	output(@"  * %@", filename);
-	
-	NSString *filePath = [sub_path stringByAppendingPathComponent:filename];
-	NSImage *originalImage = [[NSImage alloc] initWithContentsOfFile:filePath];
-	
-	NSBitmapImageRep *bitmapImageRep = [NSBitmapImageRep imageRepWithData:[originalImage TIFFRepresentation]];
-	NSColorSpace *origColorSpace = [bitmapImageRep colorSpace];
-	//NSData *colorProfileData = [origColorSpace ICCProfileData];		
-	
-	NSString *slicedName = [filename stringByDeletingPathExtension];
-	NSString *slicedPath = [sub_path stringByAppendingPathComponent:slicedName];
-	if (![fileManager createDirectoryAtPath:slicedPath withIntermediateDirectories:YES attributes:nil error:&error]) {
-	    error_output(@"createDirectoryAtPath:\"%@\" error: %@", slicedPath, error);
-	}
-	
-	slice_image(originalImage, origColorSpace, slicedName, slicedPath, @"4000");
-	
-	NSImage *halfImage = [originalImage imageScaledToFitSize:NSMakeSize(originalImage.size.width/2, originalImage.size.height/2)];
-	slice_image(halfImage, origColorSpace, slicedName, slicedPath, @"2000");
-	
-	NSImage *qtrImage = [originalImage imageScaledToFitSize:NSMakeSize(originalImage.size.width/4, originalImage.size.height/4)];
-	slice_image(qtrImage, origColorSpace, slicedName, slicedPath, @"1000");
-	
-	[originalImage release];
+    for (NSString *filename in dirlist)
+    {
+      if ([filename hasPrefix:@"."])
+      {
+        continue;
+      }
+      
+      output(@"  * %@", filename);
+      
+      NSString *filePath = [sub_path stringByAppendingPathComponent:filename];
+      NSImage *originalImage = [[NSImage alloc] initWithContentsOfFile:filePath];
+      
+      NSBitmapImageRep *bitmapImageRep = [NSBitmapImageRep imageRepWithData:[originalImage TIFFRepresentation]];
+      NSColorSpace *origColorSpace = [bitmapImageRep colorSpace];
+      //NSData *colorProfileData = [origColorSpace ICCProfileData];
+
+      NSString *slicedName = [filename stringByDeletingPathExtension];
+      NSString *slicedPath = [sub_path stringByAppendingPathComponent:slicedName];
+
+      if (![fileManager createDirectoryAtPath:slicedPath withIntermediateDirectories:YES attributes:nil error:&error])
+      {
+        error_output(@"createDirectoryAtPath:\"%@\" error: %@", slicedPath, error);
+      }
+      
+      slice_image(originalImage, origColorSpace, slicedName, slicedPath, @"4000");
+      
+      //resize image
+      NSImage *halfImage = [originalImage imageScaledToFitSize:NSMakeSize(originalImage.size.width/2, originalImage.size.height/2)];
+      slice_image(halfImage, origColorSpace, slicedName, slicedPath, @"2000");
+      
+      NSImage *qtrImage = [originalImage imageScaledToFitSize:NSMakeSize(originalImage.size.width/4, originalImage.size.height/4)];
+      slice_image(qtrImage, origColorSpace, slicedName, slicedPath, @"1000");
+      
+      [originalImage release];
     }
-    
-    [pool drain];
-    
-    return 0;
+  }
+
+  return 0;
 }
 
 int start_slicing(NSString *path)
 {
-    NSError *error = nil;
-    
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray *dirlist = [fileManager contentsOfDirectoryAtPath:path error:&error];
-    if (nil == dirlist) {
-	error_output(@"contentsOfDirectoryAtPath:\"%@\" error: %@", path, error);
+  NSError *error = nil;
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  
+  NSArray *dirlist = [fileManager contentsOfDirectoryAtPath:path error:&error];
+  if (nil == dirlist)
+  {
+    error_output(@"contentsOfDirectoryAtPath:\"%@\" error: %@", path, error);
+  }
+  
+  for (NSString *subdir in dirlist)
+  {
+    if ([subdir hasPrefix:@"."])
+    {
+      continue;
     }
+
+    NSString *sub_path = [path stringByAppendingPathComponent:subdir];
+    slice_files_at_path(sub_path);
     
-    for (NSString *subdir in dirlist) {
-	if ([subdir hasPrefix:@"."]) {
-	    continue;
-	}
-	
-	NSString *sub_path = [path stringByAppendingPathComponent:subdir];
-	slice_files_at_path(sub_path);
-	
-	NSString *productID = [subdir substringFromIndex:[subdir length]-5];
-	NSString *newDirName = [NSString stringWithFormat:@"product_%@_large", productID];
-	NSString *newPath = [path stringByAppendingPathComponent:newDirName];
-	
-	if (![fileManager moveItemAtPath:sub_path toPath:newPath error:&error]) {
-	    error_output(@"moveItemAtPath: \"%@\" toPath: \"%@\" error: %@", sub_path, newPath, error);
-	}
-	
-	output(@"%@ <- %@", newPath, sub_path);
+    NSString *productID = [subdir substringFromIndex:[subdir length] - 5];
+    NSString *newDirName = [NSString stringWithFormat:@"product_%@_large", productID];
+    NSString *newPath = [path stringByAppendingPathComponent:newDirName];
+    
+    if (![fileManager moveItemAtPath:sub_path toPath:newPath error:&error])
+    {
+      error_output(@"moveItemAtPath: \"%@\" toPath: \"%@\" error: %@", sub_path, newPath, error);
     }
-    
-    return 0;
+
+    output(@"%@ <- %@", newPath, sub_path);
+  }
+
+  return 0;
 }
 
 void usage() {
     error_output(@"SliceTool [-V|--version] [-v|--verbose] [-D|--debug] path");
 }
 
-int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    
-    int ret = 0;
-    
+int main (int argc, const char * argv[])
+{  
+  int ret = 0;
+
+  @autoreleasepool
+  {
     enable_verbose_output = YES;
     enable_debug_output = NO;
-    
+
     BOOL optShowHelp = NO;
     BOOL optShowVersion = NO;
-    
+
     static struct option optList[] = {
-	{"help",		no_argument, NULL, 'h'},
-	{"quiet",		no_argument, NULL, 'q'},
-	{"debug",		no_argument, NULL, 'D'},
-	{"version",		no_argument, NULL, 'V'},
-	{NULL,			0, NULL, 0},
+      {"help",		no_argument, NULL, 'h'},
+      {"quiet",		no_argument, NULL, 'q'},
+      {"debug",		no_argument, NULL, 'D'},
+      {"version",		no_argument, NULL, 'V'},
+      {NULL,			0, NULL, 0},
     };
-    
+
     int c;
     int ix;
-    
-    while (1) {
-	c = getopt_long(argc, (char * const *)argv, "hoqVD::", optList, &ix);
-	if (c == EOF) {
-	    break;
-	}
-	switch (c) {
-	    case 'h':
-		optShowHelp = YES;
-		break;
-	    case 'q':
-		enable_verbose_output = NO;
-		break;
-	    case 'D':
-		enable_debug_output = YES;
-		break;
-	    case 'V':
-		optShowVersion = YES;
-		break;
-	    default:
-		break;
-	}
+
+    while (1)
+    {
+      c = getopt_long(argc, (char * const *)argv, "hoqVD::", optList, &ix);
+      if (c == EOF)
+      {
+        break;
+      }
+      switch (c)
+      {
+        case 'h':
+          optShowHelp = YES;
+          break;
+        case 'q':
+          enable_verbose_output = NO;
+          break;
+        case 'D':
+          enable_debug_output = YES;
+          break;
+        case 'V':
+          optShowVersion = YES;
+          break;
+        default:
+          break;
+      }
     }
     
-    if (optShowHelp) {
-	usage();
-	ret = 1;
+    if (optShowHelp)
+    {
+      usage();
+      ret = 1;
     }
-    else if (optShowVersion) {
-	error_output(@"SliceTool version %s.", version);
+    else if (optShowVersion)
+    {
+      error_output(@"SliceTool version %s.", version);
 #ifdef DEBUG
-	error_output(@"DEBUG enabled");
+      error_output(@"DEBUG enabled");
 #endif
     }
-    else {
-	argc -= optind;
-	argv += optind;
-	
-	if (argc < 1) {
-	    usage();
-	    ret = 1;
-	}
-	else {
-	    NSString *path = [NSString stringWithCString:argv[0] encoding:NSUTF8StringEncoding];
-	    
-	    ret = start_slicing(path);
-	}
+    else
+    {
+      argc -= optind;
+      argv += optind;
+
+      if (argc < 1)
+      {
+        usage();
+        ret = 1;
+      }
+      else
+      {
+        NSString *path = [NSString stringWithCString:argv[0] encoding:NSUTF8StringEncoding];
+
+        ret = start_slicing(path);
+      }
     }
-    
-    [pool drain];
-    return ret;
+  }
+
+  return ret;
 }
